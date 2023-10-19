76da27a8b56b76509d86c80772df225e39a129e5
Silver <silver@squirl.dev>
2023-10-19 20:28:09 +0100

    foliage: implement chunking

--- src/foliage.ts
+++ src/foliage.ts
@@ -1,9 +1,13 @@
 import { buildAliasTables, sampleAliasTables } from "./alias";
 import SHADER_SOURCE from "./foliage.wgsl";
-import { Model, DrawPass, State, TAU, imageData, model, texture } from "./utils";
+import { Model, DrawPass, State, TAU, imageData, model, texture, assert } from "./utils";
 
-const INSTANCE_COUNT = 100_000;
 const GRASS_AREA = 80;
+const CHUNK_SIZE = 10;
+const DENSITY = 1250;
+
+const CHUNKS_PER_SIDE = GRASS_AREA / CHUNK_SIZE;
+const INSTANCE_COUNT = GRASS_AREA * DENSITY;
 
 const CONSTANTS = {
   scale: 2.0 / GRASS_AREA,
@@ -16,7 +20,7 @@ export class Foliage implements DrawPass {
     readonly pipeline: GPURenderPipeline,
     readonly binds: GPUBindGroup,
     readonly bladeModel: Model,
-    readonly instanceBuf: GPUBuffer,
+    readonly instanceBuffers: InstanceBuffers,
   ) {}
 
   static async create(state: State, outputFormat: GPUTextureFormat): Promise<Foliage> {
@@ -112,47 +116,137 @@ export class Foliage implements DrawPass {
       ],
     });
 
-    // Build random distribution tables
-    const heatmapData = await heatmap;
-    console.time("build alias");
-    const prob = new Float32Array(heatmapData.width * heatmapData.height);
-    const alias = new Uint32Array(heatmapData.width * heatmapData.height);
-    const max = buildAliasTables(prob, alias, heatmapData);
-    console.timeEnd("build alias");
-    // Generate random instance buffer
-    const instanceBuf = state.device.createBuffer({
-      size: INSTANCE_COUNT * 4 * 4,
-      usage: GPUBufferUsage.VERTEX,
-      mappedAtCreation: true,
-    });
-    const instanceData = new Float32Array(instanceBuf.getMappedRange());
-    const factorX = GRASS_AREA / heatmapData.width;
-    const factorY = GRASS_AREA / heatmapData.height;
-    const offset = GRASS_AREA / 2;
-    for (let instance = 0; instance < INSTANCE_COUNT; instance++) {
-      const idx = sampleAliasTables(prob, alias, Math.random());
-      const value = heatmapData.data[idx * 4];
-      const x = idx % heatmapData.width;
-      const y = Math.floor(idx / heatmapData.width);
-      instanceData[instance * 4 + 0] = x * factorX - offset;
-      instanceData[instance * 4 + 1] = y * factorY - offset;
+    const instanceBuffers = buildInstanceBuffers(state, await heatmap);
 
-      const angle = Math.random() * TAU;
-      instanceData[instance * 4 + 2] = angle;
-
-      const height = 0.4 + value * (1 / max);
-      instanceData[instance * 4 + 3] = height;
-    }
-    instanceBuf.unmap();
-
-    return new Foliage(pipeline, binds, await bladeModel, instanceBuf);
+    return new Foliage(pipeline, binds, await bladeModel, instanceBuffers);
   }
 
   draw(state: State, pass: GPURenderPassEncoder): void {
     pass.setPipeline(this.pipeline);
     pass.setBindGroup(0, this.binds);
     pass.setVertexBuffer(0, this.bladeModel.buf);
-    pass.setVertexBuffer(1, this.instanceBuf);
-    pass.draw(this.bladeModel.numVerts, INSTANCE_COUNT);
+
+    for (let y = 0; y < CHUNKS_PER_SIDE; y++) {
+      for (let x = 0; x < CHUNKS_PER_SIDE; x++) {
+        this.drawChunk(pass, x, y);
+      }
+    }
+  }
+
+  drawChunk(pass: GPURenderPassEncoder, x: number, y: number): void {
+    assert(x >= 0 && y >= 0, `negative: x=${x}, y=${y}`);
+    assert(x < CHUNKS_PER_SIDE && y < CHUNKS_PER_SIDE, `too big: x=${x}, y=${y}`);
+
+    const idx = x + y * CHUNKS_PER_SIDE;
+
+    const start = this.instanceBuffers.chunks[idx];
+    const end = this.instanceBuffers.chunks[idx + 1] ?? INSTANCE_COUNT;
+    if (start === end) {
+      return;
+    }
+
+    const offset = start * 4 * 4;
+    const size = (end - start) * 4 * 4;
+
+    pass.setVertexBuffer(1, this.instanceBuffers.data, offset, size);
+    pass.draw(this.bladeModel.numVerts, end - start);
   }
 }
+
+function buildInstanceBuffers(state: State, heatmapData: ImageData): InstanceBuffers {
+  // Build random distribution tables
+  performance.mark("build alias:start");
+  const prob = new Float32Array(heatmapData.width * heatmapData.height);
+  const alias = new Uint32Array(heatmapData.width * heatmapData.height);
+  const max = buildAliasTables(prob, alias, heatmapData);
+  performance.mark("build alias:end");
+  performance.measure("build alias", "build alias:start", "build alias:end");
+
+  // Generate random instance buffer
+  const data = state.device.createBuffer({
+    size: INSTANCE_COUNT * 4 * 4,
+    usage: GPUBufferUsage.VERTEX,
+    mappedAtCreation: true,
+  });
+  const instanceData = new Float32Array(data.getMappedRange());
+  const factorX = GRASS_AREA / heatmapData.width;
+  const factorY = GRASS_AREA / heatmapData.height;
+  const offset = GRASS_AREA / 2;
+  for (let instance = 0; instance < INSTANCE_COUNT; instance++) {
+    const idx = sampleAliasTables(prob, alias, Math.random());
+    const value = heatmapData.data[idx * 4];
+    const x = idx % heatmapData.width;
+    const y = Math.floor(idx / heatmapData.width);
+    instanceData[instance * 4 + 0] = x * factorX - offset;
+    instanceData[instance * 4 + 1] = y * factorY - offset;
+
+    const angle = Math.random() * TAU;
+    instanceData[instance * 4 + 2] = angle;
+
+    const height = 0.4 + value * (1 / max);
+    instanceData[instance * 4 + 3] = height;
+  }
+
+  const instanceChunkIdx = (idx: number) => {
+    const x = Math.floor((instanceData[idx * 4 + 0] + offset) / CHUNK_SIZE);
+    const y = Math.floor((instanceData[idx * 4 + 1] + offset) / CHUNK_SIZE);
+    assert(x >= 0 && y >= 0, `negative: idx=${idx}, x=${x}, y=${y}`);
+    assert(
+      x < CHUNKS_PER_SIDE && y < CHUNKS_PER_SIDE,
+      `too big: idx=${idx}, x=${x}, y=${y} ${CHUNKS_PER_SIDE}`,
+    );
+    return x + y * CHUNKS_PER_SIDE;
+  };
+
+  // In-place radix sort by chunk index
+  const sort = (bit: number, start = 0, end = INSTANCE_COUNT) => {
+    const mask = 1 << bit;
+    let low = start;
+    let high = end;
+    while (low < high) {
+      const chunkIdx = instanceChunkIdx(low);
+      if ((chunkIdx & mask) === 0) {
+        low++;
+      } else {
+        high--;
+        for (let i = 0; i < 4; i++) {
+          const tmp = instanceData[low * 4 + i];
+          instanceData[low * 4 + i] = instanceData[high * 4 + i];
+          instanceData[high * 4 + i] = tmp;
+        }
+      }
+    }
+    if (bit > 0) {
+      if (start < low) {
+        sort(bit - 1, start, low);
+      }
+      if (high < end) {
+        sort(bit - 1, high, end);
+      }
+    }
+  };
+  sort(Math.ceil(Math.log2(CHUNKS_PER_SIDE * CHUNKS_PER_SIDE)));
+
+  // Compute chunk offsets
+  const chunks = new Uint32Array(CHUNKS_PER_SIDE * CHUNKS_PER_SIDE);
+  let instanceIdx = 0;
+  for (let i = 0; i < chunks.length; i++) {
+    chunks[i] = instanceIdx;
+    while (instanceIdx < INSTANCE_COUNT && instanceChunkIdx(instanceIdx) <= i) {
+      instanceIdx++;
+    }
+  }
+  console.log(chunks);
+  assert(
+    instanceIdx === INSTANCE_COUNT,
+    `Did not fill chunk buffer correctly: got ${instanceIdx} instances, wanted ${INSTANCE_COUNT}`,
+  );
+
+  data.unmap();
+  return { data, chunks };
+}
+
+type InstanceBuffers = {
+  data: GPUBuffer;
+  chunks: Uint32Array;
+};
