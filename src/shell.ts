680baf872e7664165338501f2dc5e59761824cb6
Silver <silver@squirl.dev>
2023-11-01 13:52:47 +0000

    Move noise generation to compute shader

--- src/shell.ts
+++ src/shell.ts
@@ -2,10 +2,12 @@
 import SHADER_SOURCE from "./shell.wgsl";
 import { DrawPass, State } from "./utils";
 
-const SHELLS = 256;
-const CONSTANTS = {
-  shells: SHELLS,
-};
+const SHELL_COUNT = 256;
+const HEIGHT = 2.0;
+const BLADE_DENSITY = 10;
+const PLANE_SIZE = 10;
+
+const BLADE_COUNT_ACROSS = BLADE_DENSITY * PLANE_SIZE;
 
 export class Shell implements DrawPass {
   writes_depth_buffer = true;
@@ -15,7 +17,28 @@ export class Shell implements DrawPass {
     const layout = state.device.createPipelineLayout({
       bindGroupLayouts: [
         state.device.createBindGroupLayout({
-          entries: [{ binding: 0, visibility: GPUShaderStage.VERTEX, buffer: {} }],
+          entries: [
+            { binding: 0, visibility: GPUShaderStage.VERTEX, buffer: {} },
+            {
+              binding: 1,
+              visibility: GPUShaderStage.FRAGMENT,
+              texture: { sampleType: "unfilterable-float", viewDimension: "2d-array" },
+            },
+          ],
+        }),
+      ],
+    });
+
+    const computeLayout = state.device.createPipelineLayout({
+      bindGroupLayouts: [
+        state.device.createBindGroupLayout({
+          entries: [
+            {
+              binding: 0,
+              visibility: GPUShaderStage.COMPUTE,
+              storageTexture: { format: "r32float", viewDimension: "2d-array" },
+            },
+          ],
         }),
       ],
     });
@@ -28,31 +51,79 @@ export class Shell implements DrawPass {
       },
     });
 
-    const pipeline = await state.device.createRenderPipelineAsync({
-      layout,
+    const [pipeline, computePipeline] = await Promise.all([
+      state.device.createRenderPipelineAsync({
+        layout,
 
-      vertex: {
-        module: shader,
-        entryPoint: "vertex",
-        constants: CONSTANTS,
-      },
-      primitive: { topology: "triangle-strip" },
-      fragment: {
-        module: shader,
-        entryPoint: "fragment",
-        constants: CONSTANTS,
-        targets: [{ format: outputFormat }],
-      },
-      depthStencil: {
-        format: state.depthTex.format,
-        depthWriteEnabled: true,
-        depthCompare: "less",
-      },
+        vertex: {
+          module: shader,
+          entryPoint: "vertex",
+          constants: {
+            shell_count: SHELL_COUNT,
+            total_height: HEIGHT,
+            plane_size: PLANE_SIZE,
+          },
+        },
+        primitive: { topology: "triangle-strip" },
+        fragment: {
+          module: shader,
+          entryPoint: "fragment",
+          constants: {
+            blade_density: BLADE_DENSITY,
+            plane_size: PLANE_SIZE,
+          },
+          targets: [{ format: outputFormat }],
+        },
+        depthStencil: {
+          format: state.depthTex.format,
+          depthWriteEnabled: true,
+          depthCompare: "less",
+        },
+      }),
+
+      state.device.createComputePipelineAsync({
+        layout: computeLayout,
+        compute: {
+          module: shader,
+          entryPoint: "genNoiseTexture",
+          constants: {
+            shell_count: SHELL_COUNT,
+          },
+        },
+      }),
+    ]);
+
+    const noiseTexture = state.device.createTexture({
+      size: [BLADE_COUNT_ACROSS, BLADE_COUNT_ACROSS, SHELL_COUNT],
+      format: "r32float",
+      usage: GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.TEXTURE_BINDING,
     });
+
+    // Generate noise texture using compute shader
+    {
+      const binds = state.device.createBindGroup({
+        layout: computePipeline.getBindGroupLayout(0),
+        entries: [{ binding: 0, resource: noiseTexture.createView() }],
+      });
+
+      const encoder = state.device.createCommandEncoder();
+      const pass = encoder.beginComputePass();
+
+      pass.setPipeline(computePipeline);
+      pass.setBindGroup(0, binds);
+      pass.dispatchWorkgroups(BLADE_COUNT_ACROSS, BLADE_COUNT_ACROSS, SHELL_COUNT);
+
+      pass.end();
+      const cmds = encoder.finish();
+      state.device.queue.submit([cmds]);
+    }
 
     const binds = state.device.createBindGroup({
       layout: pipeline.getBindGroupLayout(0),
-      entries: [{ binding: 0, resource: { buffer: state.sceneDataBuf } }],
+      entries: [
+        { binding: 0, resource: { buffer: state.sceneDataBuf } },
+        { binding: 1, resource: noiseTexture.createView() },
+      ],
     });
 
     return new Shell(pipeline, binds);
@@ -61,6 +132,6 @@ export class Shell implements DrawPass {
   draw(state: State, pass: GPURenderPassEncoder): void {
     pass.setPipeline(this.pipeline);
     pass.setBindGroup(0, this.binds);
-    pass.draw(4, SHELLS);
+    pass.draw(4, SHELL_COUNT);
   }
 }
--- src/shell.wgsl
+++ src/shell.wgsl
@@ -4,39 +4,50 @@ struct SceneData {
 }
 
 @group(0) @binding(0) var<uniform> scene: SceneData;
+@group(0) @binding(1) var noiseTexture: texture_2d_array<f32>;
 
 struct VertexOutput {
     @builtin(position) pos: vec4f,
     @location(0) uv: vec2f,
-    @location(1) @interpolate(flat) inst: u32,
+    @location(1) @interpolate(flat) shell: u32,
 }
 
-override shells: u32;
-override blade_density: f32 = 10.0;
-override total_height: f32 = 2.0;
-override plane_size: f32 = 5.0;
+override shell_count: u32;
+override blade_density: f32;
+override total_height: f32;
+override plane_size: f32;
 
 @vertex
-fn vertex(@builtin(vertex_index) vert_idx: u32, @builtin(instance_index) inst: u32) -> VertexOutput {
-    let uv_i = vec2((vert_idx & 1u) << 1u, vert_idx & 2u);
+fn vertex(@builtin(vertex_index) vert_idx: u32, @builtin(instance_index) instance: u32) -> VertexOutput {
+    let shell = shell_count - instance - 1u; // Render from top to bottom - helps with overdraw
+    let uv_i = vec2(vert_idx & 1u, (vert_idx >> 1u) & 1u);
     let uv = vec2<f32>(uv_i);
-    let height = f32(inst) * (total_height / f32(shells));
+    let height = f32(shell) * (total_height / f32(shell_count));
     let pos = scene.mvp * vec4(uv * plane_size, height, 1.0).xzyw;
-    return VertexOutput(pos, uv, inst);
+    return VertexOutput(pos, uv, shell);
 }
 
 @fragment
-fn fragment(@location(0) uv: vec2f, @location(1) @interpolate(flat) inst: u32) -> @location(0) vec4f {
+fn fragment(@location(0) uv: vec2f, @location(1) @interpolate(flat) shell: u32) -> @location(0) vec4f {
     let scaledPos = uv * plane_size * blade_density;
     let localPos = (scaledPos - floor(scaledPos) - 0.5) * 2.0;
 
-    let x = floatHash2(vec2<u32>(scaledPos));
-    let height = f32(inst) / f32(shells);
-    let radius = 1.0 - height;
-    if x < height || dot(localPos, localPos) >= radius * radius {
+    let radius = textureLoad(noiseTexture, vec2<i32>(scaledPos), i32(shell), 0).x;
+    if dot(localPos, localPos) >= radius {
         discard;
     }
-    return vec4(height, 0.0, 0.0, 2.0);
+    return vec4(1.0 - radius, 0.0, 0.0, 2.0);
+}
+
+@group(0) @binding(0) var outputTexture: texture_storage_2d_array<r32float, write>;
+
+@compute
+@workgroup_size(1)
+fn genNoiseTexture(@builtin(global_invocation_id) pos: vec3u) {
+    let value = floatHash2(pos.xy);
+    let height = f32(pos.z) / f32(shell_count);
+    let radius = select(1.0 - height, 0.0, value < height);
+    textureStore(outputTexture, vec2<i32>(pos.xy), i32(pos.z), vec4(radius));
 }
 
 // PCG-based hash function; pareto-optimal according to https://jcgt.org/published/0009/03/02/
